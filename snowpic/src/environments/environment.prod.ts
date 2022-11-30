6f63b14ebce8bee2db55da95d3aee25147bfeb55
KT-Um <kt.um99@gmail.com>
2022-12-01 01:09:24 +0900

    refactoring

--- snowpic/src/environments/environment.prod.ts
+++ snowpic/src/environments/environment.prod.ts
@@ -9,7 +9,3 @@ export const APP_TITLE = {
 export const SUPPORT_FORMAT = {
   "format": /jpg|jpeg|png|gif|JPG|JPEG|PNG|GIF/
 };
-
-export const API_URL = {
-  "url": "http://192.168.0.50/Photo/"
-};
